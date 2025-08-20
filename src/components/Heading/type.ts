f8fbb58051971683cdd29688fa946a123ff697ab
ashley-rr <160376224+ashley-rr@users.noreply.github.com>
2025-08-20 12:45:42 -0700

    refactor: move Heading component branch from rcc-site-mk3 to rcc-site-final

---
+++ src/components/Heading/type.ts
@@ -0,0 +1,10 @@
+export type headingProps = {
+  children: React.ReactNode;
+  customStyle?: React.CSSProperties;
+  align?: "left" | "center";
+  logoPath?: string;
+  logoAlign?: "left" | "right";
+  logoSize?: number;
+}
+
+
