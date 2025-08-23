9550de1395f391ec131dbc0a042f3ee3914a50f0
Marl Jonson <marl.jonson@comcast.net>
2025-08-23 01:35:47 -0700

    Make blank ambassadors page

---
+++ src/app/(public)/ambassadors/page.tsx
@@ -0,0 +1,13 @@
+// app/public/ambassadors/page.tsx
+import React from 'react';
+import styles from './page.module.css';
+import BlogCard from '../../../components/BlogCard';
+
+export default function AmbassadorsPage() {
+  return (
+    <main className={styles.container}>
+      <h1>Meet Our Ambassadors</h1>
+      {/* Future ambassador cards will go here */}
+    </main>
+  );
+}
