9b9a3a569cb9087243b0003d35674b2511f02b08
Preethi Mohan <prmo2016@gmail.com>
2025-08-22 19:24:12 -0700

    move Tailwind styling to a css file

--- src/app/industry/page.tsx
+++ src/app/industry/page.tsx
@@ -1,93 +1,108 @@
 'use client';
 import Image from 'next/image';
 import Link from 'next/link';
+// import BackgroundGradient from '@/components/BackgroundGradient';
+// import Heading from '@/components/Heading';
+import styles from './industry.module.css';
 
 export default function IndustryPage() {
   return (
     <div>
-      <div className="px-6 sm:px-10 md:px-20 pt-5 md:pt-10">
-        <section className="flex flex-col items-center">
-          <h1 className="pb-2 sm:pb-4 md:pb-7 text-3xl sm:text-4xl md:text-5xl xl:text-[50pt]">
-            <b>Industry</b>
-          </h1>
-          <div className="flex flex-col md:flex-row items-center gap-4 xl:gap-3">
-            <p className="text-md md:text-lg text-center md:text-left xl:text-2xl flex flex-col m-0">
-              The Industry Committee actively partners with professionals and companies to bring you inspiring speaker
-              events and vibrant networking opportunities—all focused on responsible computing in action.
-              <br />
-              <br className="hidden md:flex" />
-              We&apos;re here to help you build the skills and connections you need to grow, thrive, and succeed!
-              <br />
-              <br className="" />
-              <b>
-                Interested in hosting an event with us? Reach out to us at{' '}
-                <span className="text-brand-indigo">rcc.sjsu@gmail.com</span> or fill out{' '}
-                <span className="text-brand-indigo">
-                  <Link href="#interest-form" className="hover:underline">
-                    this interest form
-                  </Link>
-                </span>{' '}
-                to get in contact!
-              </b>
-            </p>
-            <Image
-              src="/images/industry-lauren-klein.svg"
-              alt=""
-              width={250}
-              height={250}
-              className="w-[150px] h-[150px] sm:w-[200px] sm:h-[200px] md:w-[250px] md:h-[250px]  lg:w-[300px] lg:h-[300px] xl:w-[250px] xl:h-[250px]"
-            />
+      <div className={styles.industryAndPastEventsContainer}>
+        {/* Industry Section */}
+        <section className={styles.sectionContainer}>
+          {/* <BackgroundGradient color="purple" className="size-72 inline"></BackgroundGradient> */}
+          {/* <Heading
+            headingTag={'h1'}
+            children={'Industry'}
+            logoPath="/images/handshake-logo.svg"
+            logoAlign="right"
+            logoSize={7}
+          /> */}
+          <div>
+            <h1 className={styles.heading}>Industry</h1>
+            <div className={styles.industryTextAndImageContainer}>
+              <p className={styles.industryText}>
+                The Industry Committee actively partners with professionals and companies to bring you inspiring speaker
+                events and vibrant networking opportunities—all focused on responsible computing in action.
+                <br />
+                <br className={styles.hiddenBR} />
+                We&apos;re here to help you build the skills and connections you need to grow, thrive, and succeed!
+                <br />
+                <br />
+                <b>
+                  Interested in hosting an event with us? Reach out to us at{' '}
+                  <span className={styles.indigoText}>rcc.sjsu@gmail.com</span> or fill out{' '}
+                  <span className={styles.indigoText}>
+                    <Link href="#interest-form" className={styles.interestFormLink}>
+                      this interest form
+                    </Link>
+                  </span>{' '}
+                  to get in contact!
+                </b>
+              </p>
+              <Image
+                src="/images/industry-lauren-klein.svg"
+                alt=""
+                width={250}
+                height={250}
+                className={styles.industryImage}
+              />
+            </div>
           </div>
         </section>
 
-        <hr className="mt-7 mb-5 md:mt-15 md:mb-8 xl:mt-20 xl:mb-10"></hr>
+        <hr className={styles.industryAndPastEventDivider} />
 
-        <section className="flex flex-col items-center">
-          <h2 className="text-brand-indigo pb-3 sm:pb-4 md:pb-5 lg:pb-7 text-3xl sm:text-4xl md:text-5xl xl:text-[50pt]">
+        {/* Past Events Section */}
+        <section className={styles.sectionContainer}>
+          {/* <BackgroundGradient color="purple" className="size-72 inline"></BackgroundGradient> */}
+          {/* <Heading headingTag={'h2'} children={'Past Events'} /> */}
+          <h2 className={`${styles.indigoText} ${styles.heading}`}>
             <b>Past Events</b>
           </h2>
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 sm:space-x-10 md:space-x-0 md:gap-12 xl:gap-15 justify-center md:items-center lg:items-stretch text-sm md:text-lg text-center">
-            <div className="flex flex-col items-center mb-3">
+          <div className={styles.pastEventsContainer}>
+            <div className={styles.pastEvent}>
               <Image
                 src="/images/women-in-tech-panel-katherine-d-harris.svg"
                 alt=""
                 width={350}
                 height={350}
-                className="mb-2 md:mb-4 w-[200px] h-[200px] md:w-[200px] md:h-[200px] lg:w-[300px] lg:h-[300px] xl:w-[350px] xl:h-[350px]"
+                className={`${styles.indigoText} ${styles.pastEventImage}`}
               />
-              <p>
+              <p className={styles.pastEventText}>
                 <b>Women In Tech Panel</b>
                 <br></br>
                 April 16, 2025
               </p>
             </div>
-            <div className="flex flex-col items-center mb-3">
+            <div className={styles.pastEvent}>
               <Image
                 src="/images/exploring-responsible-computing-paths-panelists.svg"
                 alt=""
                 width={350}
                 height={350}
-                className="mb-2 md:mb-4 w-[200px] h-[200px] md:w-[200px] md:h-[200px] lg:w-[300px] lg:h-[300px] xl:w-[350px] xl:h-[350px]"
+                className={styles.pastEventImage}
               />
-              <p className="">
-                <b>Data Feminism with Lauren Klein</b>
+              <p className={styles.pastEventText}>
+                <b>Exploring Responsible Computing Paths</b>
                 <br></br>
-                October 17th, 2025
+                April 23rd, 2025
               </p>
             </div>
 
-            <div className="flex flex-col items-center sm:col-span-2 md:col-span-1 mb-0">
+            <div className={styles.thirdPastEvent}>
               <Image
                 src="/images/data-feminism-with-lauren-klein.svg"
                 alt=""
                 width={350}
                 height={350}
-                className="mb-2 md:mb-0 lg:mb-4 w-[200px] h-[200px] md:w-[200px] md:h-[200px] lg:w-[300px] lg:h-[300px] xl:w-[350px] xl:h-[350px]"
+                className={`${styles.pastEventImage} ${styles.dataFeminismPastEventImage}`}
               />
-              <p className="">
-                <b>Exploring Responsible Computing Paths</b>
+              <p className={styles.pastEventText}>
+                <b>Data Feminism with Lauren Klein</b>
                 <br></br>
-                April 23rd, 2025
+                October 17th, 2025
               </p>
             </div>
           </div>
@@ -96,15 +111,22 @@ export default function IndustryPage() {
         <hr className="my-4 md:my-8 xl:my-10"></hr>
       </div>
 
-      <section id="interest-form" className="flex flex-col">
-        <h2 className="text-brand-indigo pb-3 sm:pb-4 md:pb-7 text-3xl sm:text-4xl md:text-5xl xl:text-[50pt] md:px-20 text-center md:text-left">
-          <b>Interest Form</b>
-        </h2>
+      {/* Interest Form Section */}
+      <section id="interest-form" className={styles.interestFormSectionContainer}>
+        {/* <Heading
+          headingTag={'h2'}
+          children={'Interest Form'}
+          logoPath="/images/shapes-logo.svg"
+          align="left"
+          logoAlign="left"
+          logoSize={7}
+        /> */}
+        <h2 className={`${styles.indigoText} ${styles.heading} ${styles.interestFormHeading}`}>Interest Form</h2>
         <iframe
           src="https://docs.google.com/forms/d/e/1FAIpQLSfLu2YuHYDi6YdLcCEPCO2ue3o8FeCTnzrB7gadmeeSJ_0wVw/viewform?embedded=true"
           width="100%"
           height="548"
-          className="bg-[#F0EBF8]"
+          className={styles.googleFormsBackground}
         >
           Loading…
         </iframe>
