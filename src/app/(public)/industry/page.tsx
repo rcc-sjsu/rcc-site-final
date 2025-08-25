9a8d6d69f373be7e34564bff2a9af8d161472a72
Preethi Mohan <prmo2016@gmail.com>
2025-08-24 19:10:58 -0700

    comment out and remove unecessary imports

--- src/app/(public)/industry/page.tsx
+++ src/app/(public)/industry/page.tsx
@@ -1,8 +1,5 @@
 'use client';
-import Image from 'next/image';
-import Link from 'next/link';
 // import BackgroundGradient from '@/components/BackgroundGradient';
-import Heading from '@/components/Heading';
 import styles from './industry.module.css';
 import IndustryComponent from './IndustryComponent';
 import PastEventsComponent from './PastEvents';
