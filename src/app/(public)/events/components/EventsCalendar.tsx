d335a8ebc53a91f03e3a281fd534b8f2d9fd2a9a
Matthew Bernard <matthewbernard4@gmail.com>
2026-01-21 21:29:02 -0800

    Update EventsCalendar component to embed Google Calendar instead of Styled Calendar

--- src/app/(public)/events/components/EventsCalendar.tsx
+++ src/app/(public)/events/components/EventsCalendar.tsx
@@ -2,7 +2,7 @@ export default function EventsCalendar() {
   return (
     <div className="w-full overflow-hidden rounded-lg aspect-[16/9]">
       <iframe
-        src="https://embed.styledcalendar.com/#DBbT3ywMoEOIzrCaOy4E"
+        src="https://calendar.google.com/calendar/embed?src=cfded6dda19dfd3d689afd5790274ba9d55ac426e796e0d86b13e2c587cc321f%40group.calendar.google.com&ctz=America%2FLos_Angeles"
         title="Styled Calendar"
         className="w-full h-full border-none"
         data-cy="calendar-embed-iframe"
