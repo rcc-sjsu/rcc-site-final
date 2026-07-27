9681b0e4ec8ec0d3d01b15e4b8825a9d9773e580
Peter Le <officialpetermle@gmail.com>
2026-07-26 17:18:59 -0700

    feat: added new button variants

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -8,25 +8,33 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: 'bg-primary text-primary-foreground hover:bg-hover-accent',
-        outline:
-          'border-border bg-background hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50',
+        default: 
+          'bg-primary text-primary-foreground hover:bg-hover-accent',
+        'default-outline':
+          'bg-primary text-primary-foreground border-brand-dark-violet hover:bg-hover-accent',
         secondary:
           'bg-background text-primary border-primary hover:bg-hover-accent hover:text-primary-foreground hover:border-hover-accent aria-expanded:bg-secondary aria-expanded:text-secondary-foreground',
+        'secondary-outline': // the same as secondary but preserves border upon hover (secondary - hover:border-hover-accent)
+          'bg-background text-primary border-primary hover:bg-hover-accent hover:text-primary-foreground aria-expanded:bg-secondary aria-expanded:text-secondary-foreground',
+        outline: // delete?
+          'border-border bg-background hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:border-input dark:bg-input/30 dark:hover:bg-input/50',
         ghost:
           'hover:bg-muted hover:text-foreground aria-expanded:bg-muted aria-expanded:text-foreground dark:hover:bg-muted/50',
         destructive:
           'bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40',
-        link: 'text-primary underline-offset-4 hover:underline',
+        link: 
+          'text-primary underline-offset-4 hover:underline',
+        social: 
+          'bg-brand-lavender text-primary rounded-lg border-2 border-primary hover:bg-hover-accent hover:text-primary-foreground focus-visible:bg-hover-accent focus-visible:text-primary-foreground',
       },
       size: {
-        default: 'h-8 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2',
-        xs: "h-6 gap-1 rounded-[min(var(--radius-md),10px)] px-2 text-xs in-data-[slot=button-group]:rounded-lg has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3",
-        sm: "h-7 gap-1 rounded-[min(var(--radius-md),12px)] px-2.5 text-[0.8rem] in-data-[slot=button-group]:rounded-lg has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3.5",
-        lg: 'h-9 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2',
+        default: 'h-auto gap-1.5 px-6 md:px-10 py-2 md:py-3 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2', // See More ___ sizing
+        xs: "h-6 gap-1 rounded-[min(var(--radius-md),10px)] px-2 text-xs in-data-[slot=button-group]:rounded-lg has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3", // unchanged
+        sm: "h-7 gap-1 rounded-[min(var(--radius-md),12px)] px-2.5 text-[0.8rem] in-data-[slot=button-group]:rounded-lg has-data-[icon=inline-end]:pr-1.5 has-data-[icon=inline-start]:pl-1.5 [&_svg:not([class*='size-'])]:size-3.5", // unchanged
+        lg: 'h-auto gap-2 md:gap-4 px-6 py-3 md:py-4', // social media button sizing
+        xlg: 'h-8 gap-1.5 px-2.5 has-data-[icon=inline-end]:pr-2 has-data-[icon=inline-start]:pl-2', // hero CTA sizing
         icon: 'size-8',
-        'icon-xs':
-          "size-6 rounded-[min(var(--radius-md),10px)] in-data-[slot=button-group]:rounded-lg [&_svg:not([class*='size-'])]:size-3",
+        'icon-xs': "size-6 rounded-[min(var(--radius-md),10px)] in-data-[slot=button-group]:rounded-lg [&_svg:not([class*='size-'])]:size-3",
         'icon-sm': 'size-7 rounded-[min(var(--radius-md),12px)] in-data-[slot=button-group]:rounded-lg',
         'icon-lg': 'size-9',
       },
