2705867d611c827d90039ccdc41e498c1b452bcf
alisonlau10 <lau.alison.yj@gmail.com>
2026-07-31 04:29:55 -0700

    Revert "Add gradient effect to section headings"
    This reverts commit 914eab16217cff93f9806f959455461dbf4a9120.

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -24,7 +24,7 @@ export default function Heading({
         <Image src={logoPath} className={styles.logo} height={logoSize} width={logoSize} alt="" />
       )}
 
-      <span className={styles.gradientText}>{children}</span>
+      <div className={styles.heading}>{children}</div>
 
       {/* Right-aligned logo */}
       {logoPath && logoAlign == 'right' && logoSize && (
