4dc05988168ce5f0c9c7d737908c6be7391dc779
Caleb Fringer <cfringer98@gmail.com>
2026-08-02 16:35:59 -0700

    Add light tone support to Heading

--- src/components/Heading/index.tsx
+++ src/components/Heading/index.tsx
@@ -7,24 +7,27 @@ export default function Heading({
   headingTag,
   className,
   children,
+  customStyle,
   align,
+  tone = 'default',
   logoPath,
   logoAlign,
   logoSize,
 }: headingProps) {
   const HeadingTag = headingTag as keyof JSX.IntrinsicElements;
+  const toneClassName = tone === 'light' ? styles.light : '';
 
   return (
     <HeadingTag
-      style={{ justifyContent: align == 'left' ? 'left' : 'center' }}
-      className={`${styles.heading} ${className}`}
+      style={{ justifyContent: align == 'left' ? 'left' : 'center', ...customStyle }}
+      className={`${styles.heading} ${toneClassName} ${className ?? ''}`}
     >
       {/* Left-aligned logo */}
       {logoPath && logoAlign == 'left' && logoSize && (
         <Image src={logoPath} className={styles.logo} height={logoSize} width={logoSize} alt="" />
       )}
 
-      <div className={styles.heading}>{children}</div>
+      <span className={styles.headingText}>{children}</span>
 
       {/* Right-aligned logo */}
       {logoPath && logoAlign == 'right' && logoSize && (
