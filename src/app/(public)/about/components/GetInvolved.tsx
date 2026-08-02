18d227ff8959d0310f0fc74572a1b3720b4e82c5
Caleb Fringer <cfringer98@gmail.com>
2026-08-01 22:49:54 -0700

    WIP: Implement basic features of redesigned "Get Involved" section: masked background images, white text, new prose.

--- src/app/(public)/about/components/GetInvolved.tsx
+++ src/app/(public)/about/components/GetInvolved.tsx
@@ -1,55 +1,50 @@
-import Link from 'next/link';
-import BackgroundGradient from '@/components/BackgroundGradient';
 import Heading from '@/components/Heading';
-import styles from '../getinvolved.module.css';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function GetInvolved() {
   return (
-    <section id="get-involved" className={styles.container}>
-      <Heading headingTag="h2"> Get Involved</Heading>
+    <section id="get-involved" className="flex-col flex-center flex-wrap">
+      <Heading headingTag="h2">How Can You Get Involved?</Heading>
 
-      <p className={styles.description}>Want to join RCC? See which option below fits you the best.</p>
+      <p className="text-center">
+        Take a loot at what we&apos;ve done in the past and what we&apos;re doing right now so you can get an idea of
+        what you can contribute to in the future.
+      </p>
 
-      <ul className="flex flex-col lg:grid lg:grid-cols-2 auto-cols-max gap-15 md:gap-20 xl:gap-30 place-items-center justify-between w-full md:w-[75%] xl:w-full h-auto md:h-full lg:h-auto mt-5 md:mt-7 xl:mt-10 text-brand-indigo">
-        {/* Students - Become a Member */}
-        <BackgroundGradient className={styles.gradient1} color="purple" />
-        <li className="bg-white h-auto md:h-full flex flex-col sm:justify-center md:justify-normal border-3 md:border-4 border-brand-indigo rounded-2xl pb-3 shadow-[10px_10px_0_var(--color-brand-indigo)] md:shadow-[15px_15px_0_var(--color-brand-indigo)]">
-          <div className="bg-[#F2E4FE] p-5 md:p-10 rounded-t-xl border-b-3 md:border-b-4 border-brand-indigo">
-            <p className="text-2xl md:text-4xl font-semibold">Students</p>
-          </div>
-          <div className=" flex flex-col justify-center items-center h-full px-7 xl:px-15 py-5 sm:py-10 gap-5 sm:gap-8 md:gap-10">
-            <p className="text-[1rem] sm:text-xl xl:text-2xl ">
-              Are you a student interested in joining RCC? Become a member today for free!
-            </p>
-            <Link
-              href="/membership"
-              className="flex items-center justify-center bg-brand-indigo px-6 md:px-5 py-2 md:py-3 w-full sm:w-[90%] md:w-[40%] lg:w-[80%] text-lg lg:text-xl text-white rounded-lg hover:bg-brand-dark-violet focus:outline-3 focus:outline-offset-0 focus:outline-brand-dark-purple focus:bg-brand-lavender focus:text-brand-dark-purple focus:font-semibold active:outline-offset-0 active:outline-brand-dark-purple active:bg-brand-lavender active:text-brand-dark-purple active:font-semibold"
-            >
-              Become a Member
-            </Link>
-          </div>
-        </li>
+      <p className="text-center">Start now and see which option below fits you best.</p>
 
-        {/* Industry Partners - Reach to RCC via Contact Form */}
-        <BackgroundGradient className={styles.gradient2} color="purple" />
-        <li className="bg-white h-auto md:h-full lg:h-auto flex flex-col justify-center border-3 md:border-4 border-brand-indigo rounded-2xl pb-3 shadow-[10px_10px_0_var(--color-brand-indigo)] md:shadow-[15px_15px_0_var(--color-brand-indigo)]">
-          <div className="bg-[#F2E4FE] p-5 md:p-10 rounded-t-xl border-b-3 md:border-b-4 border-brand-indigo">
-            <p className="text-2xl md:text-4xl font-semibold">Industry Partners</p>
-          </div>
-          <div className="flex flex-col px-7 justify-center items-center xl:px-15 py-5 sm:py-10 gap-5 sm:gap-8 md:gap-10">
-            <p className="text-[1rem] sm:text-xl xl:text-2xl ">
-              Are you a company, industry professional, or other industry partner looking to connect with RCC? Reach out
-              to us through our contact form!
-            </p>
-            <Link
-              href="#"
-              className="flex items-center justify-center bg-brand-indigo px-6 md:px-5 py-2 md:py-3 w-full sm:w-[90%] md:w-[40%] lg:w-[80%] text-lg lg:text-xl text-white rounded-lg hover:bg-brand-dark-violet focus:outline-3 focus:outline-offset-0 focus:outline-brand-dark-purple focus:bg-brand-lavender focus:text-brand-dark-purple focus:font-semibold active:outline-offset-0 active:outline-brand-dark-purple active:bg-brand-lavender active:text-brand-dark-purple active:font-semibold"
-            >
-              Reach Out to RCC
-            </Link>
-          </div>
-        </li>
-      </ul>
+      <div className="flex flex-wrap">
+        <div
+          id="students-action"
+          className="relative overflow-hidden flex flex-col flex-1 items-center justify-center text-center "
+        >
+          <Image src="/images/picnic-social-2.jpg" alt="" className="grayscale object-cover -z-10" fill />
+          <div className="absolute inset-0 bg-brand-orange/80 -z-10" />
+          <Heading headingTag="h3" className="text-white">
+            Students
+          </Heading>
+          <p className="text-white">
+            Are you a student interested in joining RCC? Sign up and become a member today for free!
+          </p>
+          <Button>Join Us</Button>
+        </div>
+
+        <div
+          id="industry-action"
+          className="relative overflow-hidden flex flex-col flex-1 items-center justify-center text-center"
+        >
+          <Image src="/images/rcc-case-comp.png" alt="" className="grayscale object-cover -z-10" fill />
+          <div className="absolute inset-0 bg-brand-pink/80 -z-10" />
+
+          <Heading headingTag="h3">Industry</Heading>
+          <p className="text-white">
+            Are you a company, industry professional, or other industry partner? Connect with RCC through our contact
+            form!
+          </p>
+          <Button>Connect With Us</Button>
+        </div>
+      </div>
     </section>
   );
 }
