7803e576682b04cb91a438ad7ac7d8ca2d0d0aa1
Caleb Fringer <cfringer98@gmail.com>
2026-08-04 15:34:38 -0700

    Add `bg-clip-border` to buttons to remove 1px gap between border and fill.

--- src/app/(public)/about/components/GetInvolved.tsx
+++ src/app/(public)/about/components/GetInvolved.tsx
@@ -84,7 +84,8 @@ export default function GetInvolved() {
                 className={buttonVariants({
                   variant: 'inverse',
                   size: 'lg',
-                  className: 'group/action-button mt-8 h-auto rounded-md px-6 py-2 text-base font-bold md:text-lg',
+                  className:
+                    'group/action-button mt-8 h-auto rounded-md px-6 py-2 text-base font-bold md:text-lg bg-clip-border',
                 })}
               >
                 <span className={action.hoverTextClassName}>{action.buttonText}</span>
