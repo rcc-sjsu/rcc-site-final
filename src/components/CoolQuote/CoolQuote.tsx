867a16cf26a256e48eec03961970911de9fc7e7e
Peter Le <officialpetermle@gmail.com>
2026-08-09 17:07:16 -0700

    style: apply Prettier formatting

--- src/components/CoolQuote/CoolQuote.tsx
+++ src/components/CoolQuote/CoolQuote.tsx
@@ -6,19 +6,17 @@ export default function CoolQuote() {
     <section className={styles.container} aria-label="Quote from RCC founder">
       <div className={styles.rule} aria-hidden="true" />
 
-      <Image
-        src="/about/quotation_marks.svg"
-        alt=""
-        width={117}
-        height={90}
-        className={styles.quotationMarks}
-      />
+      <Image src="/about/quotation_marks.svg" alt="" width={117} height={90} className={styles.quotationMarks} />
 
       <figure className={styles.quote}>
         <blockquote>
           <p>
-            Our goal isn&rsquo;t just to study technology, but to question it, to understand its effects, and to make sure
-            it&rsquo;s applied <em><b>responsibly</b></em>.
+            Our goal isn&rsquo;t just to study technology, but to question it, to understand its effects, and to make
+            sure it&rsquo;s applied{' '}
+            <em>
+              <b>responsibly</b>
+            </em>
+            .
           </p>
         </blockquote>
         <figcaption>Julia, RCC Founder</figcaption>
