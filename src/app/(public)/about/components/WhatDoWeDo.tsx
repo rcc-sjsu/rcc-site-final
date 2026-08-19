c887d31acb97736fc6c2d5de0abc99c7df6f4a2f
amgg <16143061+adrianmgg@users.noreply.github.com>
2026-08-19 14:38:00 -0700

    CTA button gradient thing test

--- src/app/(public)/about/components/WhatDoWeDo.tsx
+++ src/app/(public)/about/components/WhatDoWeDo.tsx
@@ -2,9 +2,14 @@ import Heading from '@/components/Heading';
 import Image from 'next/image';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
+import { useId } from 'react';
 import styles from '../whatdowedo.module.css';
 
 export default function WhatDoWeDo() {
+  const g1 = useId();
+  const g2 = useId();
+  const foo = 70;
+
   return (
     <section className={styles.container}>
       <div className={cn(styles.bgBox, styles.glow, 'bg-[#FBF4FF] place-self-stretch rounded-3xl')} />
@@ -23,8 +28,29 @@ export default function WhatDoWeDo() {
           In order to make RCC relevant to all majors (and minors) and to hone their collective strengths, we&apos;ve
           created dedicated teams focused on areas like workshops, consulting, marketing, web dev, and so much more!
         </p>
-        <Link href="/ambassadors" className={cn(styles.smolbutton)}>
-          Meet Our Ambassadors
+        <Link href="/ambassadors" className={cn(styles.ctaButton, 'relative overflow-hidden')}>
+          <svg className="absolute w-full h-full bg-[var(--color-brand-pink)] inset-0">
+            <defs>
+              <radialGradient id={g1}>
+                <stop offset="0%" stopColor="var(--blurple)" />
+                <stop offset="100%" stopColor="rgb(from var(--color-brand-pink) r g b / 0)" />
+              </radialGradient>
+              <radialGradient id={g2}>
+                <stop offset="0%" stopColor="var(--yellow)" />
+                <stop offset="100%" stopColor="rgb(from var(--color-brand-pink) r g b / 0)" />
+              </radialGradient>
+            </defs>
+            <ellipse className={styles.g1} fill={`url(#${g1})`} cx={`${0 - foo}%`} cy="20%" rx={`${foo}%`} ry="300%" />
+            <ellipse
+              className={styles.g2}
+              fill={`url(#${g2})`}
+              cx={`${100 + foo}%`}
+              cy="80%"
+              rx={`${foo}%`}
+              ry="300%"
+            />
+          </svg>
+          <div className="relative">Meet Our Ambassadors</div>
         </Link>
       </div>
     </section>
