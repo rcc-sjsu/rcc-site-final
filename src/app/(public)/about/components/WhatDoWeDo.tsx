4776865d9f5772e03556a72306f28496a7dada96
amgg <16143061+adrianmgg@users.noreply.github.com>
2026-08-19 15:15:22 -0700

    with rects and linear gradients instead

--- src/app/(public)/about/components/WhatDoWeDo.tsx
+++ src/app/(public)/about/components/WhatDoWeDo.tsx
@@ -31,24 +31,17 @@ export default function WhatDoWeDo() {
         <Link href="/ambassadors" className={cn(styles.ctaButton, 'relative overflow-hidden')}>
           <svg className="absolute w-full h-full bg-[var(--color-brand-pink)] inset-0">
             <defs>
-              <radialGradient id={g1}>
+              <linearGradient id={g1} y1="0%" y2="10%">
                 <stop offset="0%" stopColor="var(--blurple)" />
                 <stop offset="100%" stopColor="rgb(from var(--color-brand-pink) r g b / 0)" />
-              </radialGradient>
-              <radialGradient id={g2}>
-                <stop offset="0%" stopColor="var(--yellow)" />
-                <stop offset="100%" stopColor="rgb(from var(--color-brand-pink) r g b / 0)" />
-              </radialGradient>
+              </linearGradient>
+              <linearGradient id={g2} y1="90%" y2="100%">
+                <stop offset="0%" stopColor="var(--color-brand-pink)" stopOpacity="0" />
+                <stop offset="100%" stopColor="var(--yellow)" />
+              </linearGradient>
             </defs>
-            <ellipse className={styles.g1} fill={`url(#${g1})`} cx={`${0 - foo}%`} cy="20%" rx={`${foo}%`} ry="300%" />
-            <ellipse
-              className={styles.g2}
-              fill={`url(#${g2})`}
-              cx={`${100 + foo}%`}
-              cy="80%"
-              rx={`${foo}%`}
-              ry="300%"
-            />
+            <rect className={styles.g1} fill={`url(#${g1})`} x="-100%" y="0%" width="100%" height="100%" />
+            <rect className={styles.g2} fill={`url(#${g2})`} x="100%" y="0%" width="100%" height="100%" />
           </svg>
           <div className="relative">Meet Our Ambassadors</div>
         </Link>
